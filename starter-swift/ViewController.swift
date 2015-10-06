2df907c1989a9eb0ab98f8062d6e085892da98dc
Mike McDonald <mpmcdonald@google.com>
2015-10-05 22:40:05 -0700

    Adding appropriate licenses, changing bootstrap to a node script

--- starter-swift/ViewController.swift
+++ starter-swift/ViewController.swift
@@ -1,6 +1,18 @@
-//
-//  ViewController.swift
-//
+/*
+ * Copyright 2015 Google Inc. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 
 import UIKit
 import Firebase
@@ -10,10 +22,10 @@ class ViewController: UIViewController {
   override func viewDidLoad() {
     super.viewDidLoad()
     // Do any additional setup after loading the view, typically from a nib.
-    
+
     // Create a Firebase ref, see: https://firebase.com/docs/ios/guide
     let ref = Firebase(url: "https://<YOUR-FIREBASE-APP>.firebaseio.com")
-    
+
     // For more info, see: https://firebase.com/docs/ios/quickstart.html
   }
 
